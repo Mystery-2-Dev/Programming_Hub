c277ef2d2516b727a46e49d7971025ecedab40b1
Vani - Chug <57788019+vanichug@users.noreply.github.com>
2021-10-03 01:35:52 +0530

    Create arraysort.java

---
+++ arraysort.java
@@ -0,0 +1,20 @@
+
+import java.util.Arrays;
+
+
+public class arraysort {
+
+	
+	public static void main(String[] args)
+	{
+		
+		int[] arr = { 13, 7, 6, 45, 21, 9, 101, 102 };
+
+		
+		Arrays.sort(arr);
+
+		
+		System.out.println("Modified arr[] : %s",
+						Arrays.toString(arr));
+	}
+}
